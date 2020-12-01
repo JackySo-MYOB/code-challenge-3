727a24b79bf7716cfdd81eb19fa192692d0c5b91
Jacky So <jacky.so@myob.com>
2020-12-01 22:29:24 +1100

    Nodejs web app

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const { version } = require('./package.json');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// Fetch git sha
+const gitsha = require('child_process')
+  .execSync('git rev-parse --short HEAD')
+  .toString().trim()
+
+// App
+const app = express();
+app.get('/version',function(req,res){
+    var myJson = {} ;
+    myJson.myapplication = [ {"version": version, "lastcommitsha": gitsha, "description": "pre-interview technical test"} ];
+	res.json(myJson);
+});
+
+app.listen(PORT, HOST);
+console.log();
